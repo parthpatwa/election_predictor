38a3d50aa187576ed50e11debe7568f6b3da9cec
= <jiteshmalipeddi92@gmail.com>
2019-03-31 14:54:33 +0530

    Differentiated between user and party

--- election_predictor/urls.py
+++ election_predictor/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from authentication import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('authentication.urls'))
+    path('auth/', include('authentication.urls')),
+    path('', views.registration_all,name='registration')
 ]
