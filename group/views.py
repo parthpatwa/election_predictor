28283dd05fba107a2addb8caa943571f079d87d0
= <jiteshmalipeddi92@gmail.com>
2019-04-06 15:04:21 +0530

    Update event and minor bug fixes

--- group/views.py
+++ group/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from group.forms import groupRegistration, eventRegistration
-from .models import Group_members, Group
+from group.forms import GroupRegistration, EventRegistration
+from .models import GroupMembers, Group, Event
 from authentication.models import Profile, Usertype, Party
 from django.db import connection
 
@@ -25,9 +25,9 @@ def groups_list(request, p_id=None):
 @login_required
 def create_group(request, p_id=None):
     if p_id:
-        form = groupRegistration()
+        form = GroupRegistration()
         if request.method == 'POST':
-            form = groupRegistration(data=request.POST)
+            form = GroupRegistration(data=request.POST)
             if form.is_valid():
                 name = form.cleaned_data['name']
                 description = form.cleaned_data['description']
@@ -36,10 +36,13 @@ def create_group(request, p_id=None):
                     cursor.execute('select authentication_party.id from authentication_party where party_id = %s',
                                    [p_id])
                     admin_id = cursor.fetchone()[0]
-
-                with connection.cursor() as cursor:
-                    cursor.execute('insert into group_group(name, description, admin_id_id) values (%s,%s,%s)',
-                                   [name, description, admin_id])
+                try:
+                    with connection.cursor() as cursor:
+                        cursor.execute('insert into group_group(name, description, admin_id_id) values (%s,%s,%s)',
+                                       [name, description, admin_id])
+                except:
+                    return render(request, 'group/create_group.html',
+                                  {'form': form, 'error': 'This group already exists'})
 
                 with connection.cursor() as cursor:
                     cursor.execute(
@@ -56,15 +59,19 @@ def update_group(request, g_id=None):
     if g_id:
         group = Group.objects.get(pk=g_id)
         if request.method == 'POST':
-            group_details = groupRegistration(request.POST, instance=group)
+            group_details = GroupRegistration(request.POST, instance=group)
             if group_details.is_valid():
-                group.name = group_details.cleaned_data['name']
-                group.description = group_details.cleaned_data['description']
-                group.save()
-                party = Party.objects.get(pk=group.admin_id_id)
-                return HttpResponseRedirect(reverse('authentication:group:group_list', args=(party.party_id,)))
+                try:
+                    group.name = group_details.cleaned_data['name']
+                    group.description = group_details.cleaned_data['description']
+                    group.save()
+                    party = Party.objects.get(pk=group.admin_id_id)
+                    return HttpResponseRedirect(reverse('authentication:group:group_list', args=(party.party_id,)))
+                except:
+                    return render(request, 'group/edit_group.html',
+                                  {'form': group_details, 'error': 'This group already exists'})
         else:
-            group_details = groupRegistration(instance=group)
+            group_details = GroupRegistration(instance=group)
             return render(request, 'group/edit_group.html', {'form': group_details})
 
 
@@ -81,14 +88,13 @@ def event_list(request, g_id=None):
 
 def create_event(request, g_id=None):
     if g_id:
-        form = eventRegistration()
+        form = EventRegistration()
         if request.method == 'POST':
-            form = eventRegistration(data=request.POST)
+            form = EventRegistration(data=request.POST)
             if form.is_valid():
                 name = form.cleaned_data['name']
                 description = form.cleaned_data['description']
                 date = form.cleaned_data['date']
-                day = form.cleaned_data['day']
                 location = form.cleaned_data['location']
                 # group_id = Group.objects.get(pk=g_id)
                 # with connection.cursor() as cursor:
@@ -97,21 +103,49 @@ def create_event(request, g_id=None):
                 # Event.objects.create(name=name, description=description,
                 #                      date=date, day=day, location=location,
                 #                      group_id=group_id)
-                with connection.cursor() as cursor:
-                    cursor.execute(
-                        'insert into group_event(name, description, location, date, day, group_id_id) values (%s, %s, %s, %s, %s, 0%s)',
-                        [name, description, location, date, day, g_id])
+                try:
+                    with connection.cursor() as cursor:
+                        cursor.execute(
+                            'insert into group_event(name, description, location, date, group_id_id) values (%s, %s, %s, %s, %s)',
+                            [name, description, location, date, g_id])
+                except:
+                    return render(request, 'group/create_event.html',
+                                  {'form': form, 'error': 'This event already exists'})
+
                 with connection.cursor() as cursor:
                     cursor.execute('select * from group_event where group_id_id = %s', [g_id])
                     event = cursor.fetchall()
                 return render(request, 'group/event_list.html', {'event': event, 'g_id': g_id})
+
         else:
             return render(request, 'group/create_event.html', {'form': form, })
 
 
+def update_event(request, event_id):
+    if event_id:
+        event = Event.objects.get(pk=event_id)
+        if request.method == 'POST':
+            event_details = EventRegistration(request.POST, instance=event)
+            if event_details.is_valid():
+                try:
+                    event.name = event_details.cleaned_data['name']
+                    event.description = event_details.cleaned_data['description']
+                    event.location = event_details.cleaned_data['location']
+                    event.date = event_details.cleaned_data['date']
+                    event.save()
+                    return event_list(request, event.group_id_id)
+                except:
+                    return render(request, 'group/edit_event.html',
+                                  {'form': event_details, 'error': 'This event already exists'})
+
+        else:
+            event_details = EventRegistration(instance=event)
+            return render(request, 'group/edit_event.html', {'form': event_details})
+
+
 def members_list(request, g_id=None):
     if g_id:
-        group_members = Group_members.objects.filter(group_id_id=g_id, status=True)
+        group_members = GroupMembers.objects.filter(group_id_id=g_id, status=True)
         user_id = []
         for i in group_members:
             user_id.append(i.user_id_id)
@@ -122,7 +156,7 @@ def members_list(request, g_id=None):
 
 def add_group_members(request, g_id=None):
     if g_id:
-        group_members = Group_members.objects.filter(group_id_id=g_id)
+        group_members = GroupMembers.objects.filter(group_id_id=g_id)
         user_id = []
         for i in group_members:
             user_id.append(i.user_id_id)
@@ -134,13 +168,13 @@ def add_group_members(request, g_id=None):
 
 def request_member(request, g_id=None, u_id=None):
     if g_id and u_id:
-        Group_members.objects.create(user_id_id=u_id, group_id_id=g_id)
+        GroupMembers.objects.create(user_id_id=u_id, group_id_id=g_id)
     return HttpResponseRedirect(reverse('authentication:group:add_group_members', args=(g_id,)))
 
 
 def requested_members(request, g_id=None):
     if g_id:
-        group_members = Group_members.objects.filter(group_id_id=g_id, status=False)
+        group_members = GroupMembers.objects.filter(group_id_id=g_id, status=False)
         user_id = []
         for i in group_members:
             user_id.append(i.user_id_id)
@@ -150,7 +184,7 @@ def requested_members(request, g_id=None):
 
 def delete_request(request, g_id=None, u_id=None):
     if g_id and u_id:
-        instance = Group_members.objects.get(user_id_id=u_id, group_id_id=g_id)
+        instance = GroupMembers.objects.get(user_id_id=u_id, group_id_id=g_id)
         instance.delete()
     return HttpResponseRedirect(reverse('authentication:group:requested_members', args=(g_id,)))
 
@@ -158,7 +192,7 @@ def delete_request(request, g_id=None, u_id=None):
 def user_groups(request, u_id=None):
     if u_id:
         u_id = Profile.objects.get(profile__user_id=u_id)
-        group_members = Group_members.objects.filter(user_id=u_id, status=True)
+        group_members = GroupMembers.objects.filter(user_id=u_id, status=True)
         group_id = []
         for i in group_members:
             group_id.append(i.group_id_id)
@@ -175,7 +209,7 @@ def user_groups(request, u_id=None):
 def user_requested(request, u_id=None):
     if u_id:
         u_id = Profile.objects.get(profile__user_id=u_id)
-        group_members = Group_members.objects.filter(user_id=u_id, status=False)
+        group_members = GroupMembers.objects.filter(user_id=u_id, status=False)
         group_id = []
         for i in group_members:
             group_id.append(i.group_id_id)
@@ -185,7 +219,7 @@ def user_requested(request, u_id=None):
 
 def user_accept(request, g_id=None, u_id=None):
     if g_id and u_id:
-        instance = Group_members.objects.get(user_id_id=u_id, group_id_id=g_id)
+        instance = GroupMembers.objects.get(user_id_id=u_id, group_id_id=g_id)
         instance.status = True
         instance.save()
     return HttpResponseRedirect(reverse('authentication:group:user_requested', args=(request.user.pk,)))
@@ -193,6 +227,6 @@ def user_accept(request, g_id=None, u_id=None):
 
 def user_decline(request, g_id=None, u_id=None):
     if g_id and u_id:
-        instance = Group_members.objects.get(user_id_id=u_id, group_id_id=g_id)
+        instance = GroupMembers.objects.get(user_id_id=u_id, group_id_id=g_id)
         instance.delete()
     return HttpResponseRedirect(reverse('authentication:group:user_requested', args=(request.user.pk,)))
