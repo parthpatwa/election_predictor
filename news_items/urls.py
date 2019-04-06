530a6c141c607947d81c93e945fe0765418ece5d
= <jiteshmalipeddi92@gmail.com>
2019-04-06 23:19:06 +0530

    News based on user location

--- news_items/urls.py
+++ news_items/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     url(r'^feeds/new', views.new_feed, name='feed_new'),
     url(r'^feeds/', views.feeds_list, name='feeds_list'),
     url(r'^saved_queries', views.saved_queries, name='saved_queries'),
+    url(r'^regional_news', views.regional_news, name='regional_news'),
+    url(r'^national_news', views.national_news, name='national_news'),
+
 ]
