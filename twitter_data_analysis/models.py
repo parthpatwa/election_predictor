8c2e37e377e7e5691e03e81a09ddf70d038d45bc
Parth Patwa <parthpatwa1@gmail.com>
2019-04-07 14:51:52 +0530

    minor bug fixes in twitter app

--- twitter_data_analysis/models.py
+++ twitter_data_analysis/models.py
@@ -3,6 +3,6 @@ from authentication.models import Party
 # Create your models here.
 class tweets(models.Model):
 	hashtag = models.CharField(max_length = 200);
-	tweet = models.TextField(blank = True, NUll = True);
+	tweet = models.TextField(blank = True, null = True);
 	party = models.ForeignKey(Party,on_delete=models.CASCADE)
 	is_pro = models.BooleanField(default = False)
