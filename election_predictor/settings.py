7a1a8380e2a9c9aaad1dd7e9d3b260a76d96e10c
SatishKumar9 <satishkumarnarava9@gmail.com>
2019-04-30 19:36:24 +0530

    nav magicline for party navbar

--- election_predictor/settings.py
+++ election_predictor/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'election_predictordb',
         'USER': 'root',
-        'PASSWORD': 'mysqlroot',
+        'PASSWORD': 'snowysatish1248',
         'HOST': 'localhost',
         'PORT': '',
     }
