8770086c9d2985765bbf025fb146fe24eca8a315
Parth Patwa <parthpatwa1@gmail.com>
2019-04-30 23:22:27 +0530

    data analysis for tweets

---
+++ twitter_data_analysis/data_analysis.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import random
+import numpy
+from nltk.corpus import stopwords
+import os
+
+
+stops = set(stopwords.words("english"))    
+
+def get_len_min_max_mean(lst):
+    total = len(lst)
+    avgl = lst.mean()
+    return total , avgl
+
+def get_most_frequent(n,lst):
+    wordcount = list(pd.Series(' '.join(lst).split()).value_counts()[:n].keys())
+    return wordcount
+
+def get_stats_for_df(dataF):
+    reviews, avg = get_len_min_max_mean(dataF['text length'])#send to django
+    wc = get_most_frequent(50,dataF['text'])#send to django
+    df_stats = {'count':reviews,'avg_text_len':avg,'freq_words' : wc}
+    return dict(df_stats)
+
+
+def get_stats(complete, name):
+    complete['text length'] = complete['tweet'].apply(len)
+    opp_party = complete[complete['party'] != name]
+    party = complete[complete['party'] == name]
+    mvdf = party.copy()
+    mvpdf = mvdf[mvdf['polarity'] == 'p']
+    mvndf = mvdf[mvdf['polarity'] == 'n']
+    pos_stats = get_stats_for_df(mvpdf)
+    neg_stats = get_stats_for_df(mvndf)
+    opp_pos = opp_party[opp_party['polarity'] == 'p']
+    opp_neg = opp_party[opp_party['polarity'] == 'n']
+    opp_name = list(opp_party['party'])[0]
+    return pos_stats,neg_stats, opp_pos, opp_neg, opp_name
--- twitter_data_analysis/views.py
+++ twitter_data_analysis/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required
-from .data_analysis import get_stats,get_fav
-#from one_page.models import *
+from .data_analysis import get_stats
 import pandas as pd
-#from credits.models import AccountBalance,Statement
+from authentication.models import Usertype, Party
 from django.http import HttpResponse
 import uuid
 
@@ -17,42 +16,42 @@ def main_view(request):
 
 def stats(request):
     if request.method == 'POST':
-        temp = AccountBalance.objects.get(user=request.user)
-        if float(temp.balance) > 200:
-            complete = pd.read_csv(dct)
-
-            total_stats,pos_stats,neg_stats = get_stats(complete,name,option)#send to django
-            posper = pos_stats['count']/total_stats['count']*100
-            negper = neg_stats['count']/total_stats['count']*100
-            context = {'new_count':unlabeled_count,'posper':posper,'negper':negper,
-            'total_stats':total_stats,'pos_stats':pos_stats,'neg_stats':neg_stats,
-            'type':option,'name':name}
-            AccountBalance.objects.filter(user=request.user).update(balance=temp.balance-200)
-            Statement.objects.create(user=request.user,transaction_id="PSA"+uuid.uuid4().hex[:9].upper(),amount=200)
+    	usertype = Usertype.objects.get(user_id = request.user.pk)
+    	if usertype.is_party:
+    		profile = Party.objects.get(party__user_id = usertype.pk)
+        	temp = profile.credit_amount
+        	bal = float(temp) - float(200)
+        if float(bal) >= 0:
+        	name = profile.name
+            complete = pd.read_csv('tweets/cleaned_political_tweets.csv')
+            pos_stats,neg_stats, opp_pos_count, opp_neg_count , opp_name = get_stats(complete,name)#send to django
+            pos_avg_len = pos_stats['avg_text_len']
+            neg_avg_len = neg_stats['avg_text_len']
+            party_pos_count = pos_stats['count']
+            party_neg_count = neg_stats['count']
+            g1_y = [party_pos_count,party_neg_count]
+            g1_x = ['p','n']
+            g2_y = [party_pos_count,opp_pos_count]
+            g2_x = [name , opp_name]
+            g3_y = [party_neg_count,opp_neg_count]
+            g3_x = [name , opp_name]
+            g4_y = [party_pos_count+party_neg_count, opp_pos_count+opp_neg_count]
+            g4_x = [name , opp_name]
+            g1_dct = {'x': g1_x , 'y' : g1_y}
+            g2_dct = {'x': g2_x , 'y' : g2_y}
+            g3_dct = {'x': g3_x , 'y' : g3_y}
+            g4_dct = {'x': g4_x , 'y' : g4_y}
+            pos_words = pos_stats['freq_words']
+            neg_words = neg_stats['freq_words']
+            context = {'g1':g1_dct,'g2':g2_dct,'g3':g3_dct,'g4':g4_dct,'pos_avg_lev':pos_avg_lev , 
+            'profile':profile, 'neg_avg_len' : neg_avg_len, 'pos_words': pos_words, 'neg_words':neg_words}
+            profile.credits_amount = bal
+            profile.save()
             return render(request, 'dataAnalysis/graphs.html', context)
         else:
             return HttpResponse('<html><head><script>alert("You dont have sufficient funds");window.location="/data_analysis";</script></head></html>')
     else:
         #return HttpResponse(status = 400)
         return redirect('data_analysis')
-def get_title(gender,rtype):
-    option1 = options[rtype]
-    lst = option1.objects.all()#list of movies
-    names = []
-    genders = []
-    polarities = []
-    for i in lst:
-        rev_objs = i.labeled.all()
-        fields = dict(i.get_fields())
-        for j in rev_objs:
-            names.append(i.title)
-            if j.polarity:
-                polarities.append('p')
-            else:
-                polarities.append('n')
-            genders.append(j.user.userprofile.gender)
-    dct = {rtype:names,'polarity':polarities,'gender':genders}
-    complete = pd.DataFrame(dct)
-    title = get_fav(complete,gender,rtype)
-    return title
+
 
