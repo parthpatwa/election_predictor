04c28c204526db223229c557e3971bf3f9f2641c
Manoj Kumar Manyala <manojmanyala@gmail.com>
2019-05-01 11:33:18 +0530

    added location analysis

--- election_predictor/settings.py
+++ election_predictor/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'election_predictordb',
         'USER': 'root',
-        'PASSWORD': 'jitesh123',
+        'PASSWORD': 'mysqlroot',
         'HOST': 'localhost',
         'PORT': '',
     }
